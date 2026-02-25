512837ef32050bb98bc03f11ca23bbf3a2c4a14b
Sanchali <sanchali280705@gmail.com>
2026-02-25 17:09:53 +0530

    node js

---
+++ index.js
@@ -0,0 +1,3 @@
+const generateRandomNumber = require('./utils');
+
+console.log(`random number ${generateRandomNumber()}`);
---
+++ utils.js
@@ -0,0 +1,5 @@
+function generateRandomNumber (){
+    return Math.floor(Math.random() * 1000) + 1;
+}
+
+module.exports = generateRandomNumber;
