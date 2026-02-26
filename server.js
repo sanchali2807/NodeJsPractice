d4e75ddde5fd8c219ba8bbc436e9bd6feb2c88b4
Sanchali <sanchali280705@gmail.com>
2026-02-26 13:05:35 +0530

    fs

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const port = process.env.PORT;
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+// get current path
+// --filename get the current file path
+// __dirname current path of directory
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(__dirname,__filename);
 
 const server = http.createServer((req,res)=>{
-    
+    try{
+        if(req.method == 'GET'){
+            if(req.url === '/'){
+                res.writeHead(200,{'Content-type' : 'text/html'});
+                res.end('<h1>HOMEPAGE</h1>');
+            }else if(req.url === '/about'){
+                 res.writeHead(200,{'Content-type' : 'text/html'});
+                res.end('<h1>About</h1>');
+            }else{
+                 res.writeHead(404,{'Content-type' : 'text/html'});
+                res.end('<h1>NOT FOUND</h1>');
+            }
+        }else{
+            throw new error('method not allowed');
+        }
+    }catch(error){
+         res.writeHead(500,{'Content-type' : 'text/html'});
+                res.end('<h1>Server error</h1>');
+    }
     // console.log(req.url);
     // console.log(req.method);
     // res.write('hello world');
@@ -12,3 +39,4 @@ const server = http.createServer((req,res)=>{
 server.listen(port,()=>{
     console.log(`server running on port ${port}`);
 })
+ 
