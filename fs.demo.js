a3185c9c6d9dccd9a925e498b1eaa9ab3d491c38
Sanchali <sanchali280705@gmail.com>
2026-03-02 11:46:09 +0530

    req.on

--- fs.demo.js
+++ fs.demo.js
@@ -5,7 +5,12 @@ import fs from 'fs';
 // callback version is the default
 
 // to read the file we need three things the location of the file then utf 8 then then callback so it is synchronous so theat when the file is read  
-fs.readFile('/test.txt','utf-8',(err,data)=>{
+fs.readFile('./test.txt','utf-8',(err,data)=>{
     if(err)throw err;
     console.log(data);
 });
+
+
+// read file sync synchronous version 
+const data = fs.readFileSync('./test.txt','utf-8');
+console.log(data);
