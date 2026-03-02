4103d8a2dd7f333eb8689035512a86ecc6f5be4d
Sanchali <sanchali280705@gmail.com>
2026-03-02 23:03:43 +0530

    g

--- pathDemo.js
+++ pathDemo.js
@@ -27,3 +27,8 @@ const __dirname = path.dirname(__filename);
 
 const filePath2 = path.join(__dirname,'dir1','dir2','test.txt');
 console.log(filePath2);
+
+
+// resolve() -- same as join 
+const filePath3 = path.resolve(__dirname,'dir1','dir2','test.txt');
+console.log(filePath3);
